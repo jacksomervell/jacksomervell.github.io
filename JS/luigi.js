52ac5c41f79aef3ce6c5e16dfc45003ce9b2228d
Jack Somervell <jacksomervell@gmail.com>
2015-08-23 21:44:04 +0100

    sorted the ghost deaths finally. Onto yoshi deaths

--- JS/luigi.js
+++ JS/luigi.js
@@ -10,30 +10,32 @@
 
 $(document).ready(function(){
 
-  var currentScore = 1
+//global vairable:
+  var currentScore = 0
   var ghostImage = $(".ghost") 
+  var currentTop = parseFloat(ghostImage.css('top'));
+  var yoshiImage = $(".yoshi")
  //adding event listeners to the objects:
+  //this one animates the ghost to die:
+    ghostImage.on('click',function(){
+      ghostImage.attr('src',"../project1/Images/Boo2.png");
+      clearInterval(moveTheGhost);
+      currentScore = currentScore + 1;
+      console.log(currentScore);
+      ghostImage.animate({
+        opacity: 0,
+        top: "+=400",
+      }, 1000, function() {
+    // Animation complete.
+  }); 
+});
 
-   ghostImage.on('click',function(){
-     ghostImage.attr('src',"../project1/Images/Boo2.png");
-     clearInterval(moveTheGhost);
-     deadGhost();
-     setInterval(deadGhost, 1)
-     currentScore = currentScore + 1
-     var currentTop = parseFloat(ghostImage.css('top'));
-     if (currentTop > 200){
-      clearInterval(deadGhost);
-      }
-  })
-
-//when the ghost dies:
-
-  function deadGhost(){
-    var currentTop = parseFloat(ghostImage.css('top'));
-    var newTop = currentTop + 1.5;
-    ghostImage.css({'top': newTop});
-    }
-
+    //this one makes the yoshis die:
+    yoshiImage.on('click', function(){
+      yoshiImage.attr('src', "../project1/Images/deadYoshi.gif")
+      currentScore = currentScore - 1;
+      console.log(currentScore)
+    })
 
 
  //making the ghost move:
@@ -77,5 +79,6 @@ $(document).ready(function(){
 
  var moveTheGhost = setInterval(floatGhost, 5);
  
+
 });
 
