cfbe15cd9380d54cc266f92968e5392aa7b185de
Jack Somervell <jacksomervell@gmail.com>
2015-08-24 12:08:50 +0100

    added the luigi cursor and sorted the random ghost restarting

--- JS/luigi.js
+++ JS/luigi.js
@@ -1,11 +1,3 @@
-//animate first boo
-// function moveBoo(){
-//   var newLeft = 10 * $(window).width();
-//   var newHeight = 10 * $(window).height();
-//   $(".ghost").css({'left': newLeft, 'top': newHeight});
-// };
-
-
 
 
 $(document).ready(function(){
@@ -13,16 +5,22 @@ $(document).ready(function(){
 //global vairable:
   var currentScore = 0
   var ghostImage = $(".ghost") 
+  var ghostImageTwo = $(".ghostTwo")
   var currentTop = parseFloat(ghostImage.css('top'));
   var yoshiImage = $(".yoshi")
   var ghostsEscaped = 0
 
+  $('#container').css( 'cursor', 'url(../project1/Images/LPMicon.png), auto' );
+//make sure thos ghosts keep on coming:
+// function ghostsComing(){
+//   $('#container').prepend('<img class = "ghost" src = "../project1/Images/Boo1flipped.png" />');
+//   floatGhost();
+//   }
 
-  //update the score
-
+// var addGhosts = setInterval(ghostsComing, 5000 - (currentScore * 100)); 
 
  //adding event listeners to the objects:
-  //this one animates the ghost to die:
+  //this one animates the ghost from the left to die:
     ghostImage.on('click',function(){
       ghostImage.attr('src',"../project1/Images/Boo2.png");
       clearInterval(moveTheGhost);
@@ -31,15 +29,31 @@ $(document).ready(function(){
       console.log(currentScore);
       ghostImage.animate({
         opacity: 0,
-        top: "+=400",
-      }, 2000, function() {
-    // Animation complete.
+        top: "+=200",
+      }, 500, function() {
+      $('.ghostImage').removeAttr('style');
     }); 
   });
 
+//ghost from the right to die:
+ghostImageTwo.on('click',function(){
+      ghostImageTwo.attr('src',"../project1/Images/Boo2.png");
+      clearInterval(moveTheGhost);
+      currentScore = currentScore + 1;
+      $("h1").text("Score: " + currentScore); //this updates the score
+      console.log(currentScore);
+      ghostImageTwo.animate({
+        opacity: 0,
+        top: "+=400",
+      }, 2000, function() {
+    }); 
+      ghostImage.css({'left' : -100, 'top': (Math.random() *200)});
+      moveTheGhost();
+  });
     //this one makes the yoshis die:
     yoshiImage.hover (function(){
       yoshiImage.attr('src', "../project1/Images/deadYoshi.gif")
+      clearInterval(moveYoshi);
       currentScore = currentScore - 1;
       $("h1").text("Score: " + currentScore); //this updates the score
       yoshiImage.css("width", "10%");
@@ -48,12 +62,19 @@ $(document).ready(function(){
         opacity: 0,
         bottom: "+=400",
       }, 3000, function() {
-    // Animation complete.
+        
         }); 
     })
 
+//making images appear randomly at different heights:
 
- //making the ghost move:
+        // var heightArray = [1, 2, 3]
+        // var startTop = heightArray[Math.floor(Math.random()*heightArray.length)];
+        // var startTop = (startTop * 100);
+       
+
+
+//making the first ghost move:
 
   function floatGhost(){
 
@@ -82,13 +103,14 @@ $(document).ready(function(){
         var upFloat = currentTop - 1;
         ghostImage.css({'top': upFloat})
         }
-
-  //these bits stop the ghost:
-      if (currentLeft > 600){
-            clearInterval(moveTheGhost);
-      }
-  }
-
+        
+        //this bit stops the ghost:
+      if (currentLeft > 550){
+            ghostImage.css({'left' : -100, 'top': (Math.random() *200)});
+              } 
+            // ghostImage.css({'display': 'none'})
+        }
+    
 //making yoshi move and stop in the same way:
 
   function floatYoshi(){
@@ -100,21 +122,15 @@ $(document).ready(function(){
       // console.log(currentTop);
       var newRight = currentRight + 1; 
       yoshiImage.css({'right': newRight});
-      if (currentRight > 150){
+      if (currentRight > 30){
             clearInterval(moveYoshi);
+            // yoshiImage.css({'display': 'none'});
       }
     }  
 
  var moveTheGhost = setInterval(floatGhost, 10);
  var moveYoshi = setInterval(floatYoshi, 25);
 
- ///this iss an attempt, doesnt seem to work 
-
- //get the boo image to appear at random heights
-
-$('#container').on
-
 
 
 });
-
