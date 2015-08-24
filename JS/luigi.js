7c3cd52f388bb294680e9bc76c3141f4f80dcf1f
Jack Somervell <jacksomervell@gmail.com>
2015-08-24 05:38:34 +0100

    also sorted yoshi animation

--- JS/luigi.js
+++ JS/luigi.js
@@ -15,31 +15,45 @@ $(document).ready(function(){
   var ghostImage = $(".ghost") 
   var currentTop = parseFloat(ghostImage.css('top'));
   var yoshiImage = $(".yoshi")
+  var ghostsEscaped = 0
+
+
+  //update the score
+
+
  //adding event listeners to the objects:
   //this one animates the ghost to die:
     ghostImage.on('click',function(){
       ghostImage.attr('src',"../project1/Images/Boo2.png");
       clearInterval(moveTheGhost);
       currentScore = currentScore + 1;
+      $("h1").text("Score: " + currentScore); //this updates the score
       console.log(currentScore);
       ghostImage.animate({
         opacity: 0,
         top: "+=400",
-      }, 1000, function() {
+      }, 2000, function() {
     // Animation complete.
-  }); 
-});
+    }); 
+  });
 
     //this one makes the yoshis die:
-    yoshiImage.on('click', function(){
+    yoshiImage.hover (function(){
       yoshiImage.attr('src', "../project1/Images/deadYoshi.gif")
       currentScore = currentScore - 1;
-      console.log(currentScore)
+      $("h1").text("Score: " + currentScore); //this updates the score
+      yoshiImage.css("width", "10%");
+      console.log(currentScore);
+      yoshiImage.animate({
+        opacity: 0,
+        bottom: "+=400",
+      }, 3000, function() {
+    // Animation complete.
+        }); 
     })
 
 
  //making the ghost move:
-
 
   function floatGhost(){
 
@@ -73,12 +87,34 @@ $(document).ready(function(){
       if (currentLeft > 600){
             clearInterval(moveTheGhost);
       }
+  }
 
-      
- }
+//making yoshi move and stop in the same way:
 
- var moveTheGhost = setInterval(floatGhost, 5);
- 
+  function floatYoshi(){
+    var currentLeft = parseInt(yoshiImage.css('left'));
+    var currentRight = parseInt(yoshiImage.css('right'));
+    var currentTop = parseFloat(yoshiImage.css('top'));
+    var currentBottom = parseInt(yoshiImage.css('bottom'));
+      // console.log(currentLeft);
+      // console.log(currentTop);
+      var newRight = currentRight + 1; 
+      yoshiImage.css({'right': newRight});
+      if (currentRight > 150){
+            clearInterval(moveYoshi);
+      }
+    }  
+
+ var moveTheGhost = setInterval(floatGhost, 10);
+ var moveYoshi = setInterval(floatYoshi, 25);
+
+ ///this iss an attempt, doesnt seem to work 
+
+ //get the boo image to appear at random heights
+
+$('#container').on
+
+
 
 });
 
